a79f2541c759a6c3068a4ccd11d1572178dc0cb5
Edu <particularspam@gmail.com>
2022-05-31 17:36:06 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Eduardo";
+
+// declarar variables
+var edad;
+// inicializar variables
+edad = 30;
+
+edad
+
+var elementos = ["Hola", "mundo"];
+
+var persona = {
+    nombre: "Eduardo",
+    apellido: "Gonzalez",
+    edad: 30
+}
+
+persona;

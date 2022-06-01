309c0f52eb6487d6d6ca07483ae3f2345424cb32
Edu <particularspam@gmail.com>
2022-06-01 16:24:08 -0500

    Coercion

---
+++ coercion.js
@@ -0,0 +1,14 @@
+var a = 20;
+//undefined
+var b = a + "";
+//undefined
+var c = String(a)
+//undefined
+typeof c
+//'string'
+console.log(c);
+//undefined
+var d = Number(c);
+//undefined
+typeof d
+//'number'

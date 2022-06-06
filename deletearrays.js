82910356343d27c51b47e6c84eb83a3f29aefe1d
Edu <particularspam@gmail.com>
2022-06-06 17:17:51 -0500

    Delete Arrays

---
+++ deletearrays.js
@@ -0,0 +1,23 @@
+// --- SHIFT ---
+
+//Creamos el array
+let array = [1,2,3,4,5]
+console.log(array)
+
+// Aplicamos .shift()
+let shiftArray = array.shift()
+
+//Revisamos. El output debe de ser [2,3,4,5]
+console.log(shiftArray)
+
+// --- POP ---
+
+//Creamos el array
+let array2 = [1,2,3,4,5]
+console.log(array2)
+
+// Aplicamos .shift()
+let shiftArray2 = array2.pop()
+
+//Revisamos. El output debe de ser [1,2,3,4]
+console.log(shiftArray2)

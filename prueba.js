29e8c1c56b1ac0ed300835a47d8cdcc711c00e28
Edu <particularspam@gmail.com>
2022-06-07 00:37:17 -0500

    Prueba

---
+++ prueba.js
@@ -0,0 +1,19 @@
+/* 
+    Bonus: si ya eres una experta o experto en el lenguaje,
+    te desafío a comentar cómo replicar este comportamiento
+    con arrays y un solo condicional. 😏
+*/
+
+const Suscription = [
+    a = "Free",
+    b = "Basic",
+    c = "Expert",
+    d = "ExpertPlus"
+];
+
+if (Suscription[a,b,c,d] === "Free") {
+    console.log("Solo puedes tomar los cursos gratis");
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+}
